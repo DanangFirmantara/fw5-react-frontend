172ee62c48894e6b0bf572ce4815a4a9c81265eb
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-24 22:33:06 +0700

    feat : add profile

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -65,3 +65,38 @@ export const getUser = (token) =>{
 		}
 	}
 }
+
+export const doEditProfile = (dataChanged, token) =>{
+	return async (dispatch) =>{
+		try{
+			dispatch({ type: USER_SETLOADING })
+			dispatch({ type: USER_CLEARERROR })
+			dispatch({ type: USER_CLEARSUCCESS })
+			const param = new URLSearchParams(dataChanged)
+			const { data } = await http(token).patch('/users', param) 
+			dispatch({
+				type : USER_GETDATA,
+				payload : data.results
+			})
+			dispatch({
+				type : USER_SETSUCCESS,
+				payload : data.message
+			})
+			dispatch({ type: USER_CLEARLOADING })
+		} catch(err){
+			let payload = ''
+			if(err.response){
+				payload = err.response.data.message
+			} else{
+				payload = err.message
+			}
+			dispatch({
+				type: USER_SETERROR,
+				payload: payload
+			})
+			dispatch({
+				type: USER_CLEARLOADING
+			})
+		}
+	}
+}
