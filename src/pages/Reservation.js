506a06f367545776fe2a5e2fc4979987e6203d33
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-24 21:18:10 +0700

    feat : add reservation redux

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,35 +1,36 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import LayoutHome from '../components/LayoutHome'
-import { createReservation} from '../redux/actions/reservation'
 import defaultImage from '../assets/image/image 6.png'
 
 export const Reservation = () => {
-	const vehicle = useSelector(state=>state.vehicle)
+	const vehicle = useSelector(state=>state.vehicle )
+	const reservation = useSelector(state => state.reservation )
 	const counter = useSelector(state=>state.counter)
 	const auth = useSelector(state=>state.auth)
-	const reservation = useSelector(state=>state.reservation)
 	const [vehiclesServe, setVehicleServe] = useState({})
-	const total = counter.num * vehiclesServe.price
 	const navigate = useNavigate()
-
+	const total = reservation.quantity * vehicle.dataDetail.price
 	const dispatch = useDispatch()
-	useEffect(() => {
-		setVehicleServe(vehicle.results[0])
-	}, [])
+	// useEffect(() => {
+	// 	setVehicleServe(vehicle.results[0])
+	// }, [])
 
-	const onReservation = async(event)=>{
+	const onReservation = (event)=>{
 		event.preventDefault()
 		const rentStartDate = event.target.elements['rentStartDate'].value
 		const rentEndDate = event.target.elements['rentEndDate'].value
-		const userId = auth.userData[0].id
-		const quantity = counter.num
-		const prepayment = 0
-		const vehicleId = vehiclesServe.id
-		const data = {rentEndDate, rentStartDate, userId, prepayment, quantity, vehicleId}
-		dispatch(createReservation(data))
-		navigate('/payment')
+		// const userId = auth.userData[0].id
+		// const quantity = counter.num
+		// const prepayment = 0
+		// const data = {rentEndDate, rentStartDate, userId, prepayment, quantity, vehicleId}
+		const data = {rentStartDate, rentEndDate}
+		console.log(data)
+		console.log('reservation')
+		// dispatch(createReservation(data))
+		// navigate('/payment')
 	}
 	
 	const goBack = () =>{
@@ -45,27 +46,21 @@ export const Reservation = () => {
 						<button className="fa-solid fa-chevron-left icon dark fs-0 me-3" onClick={goBack}></button>
 						<div className="fs-1 fw-bold text-dark">Reservation</div>
 					</div>
-					{reservation.isError &&
-						<div className="alert button-third shadow-dark alert-dismissible fade show text-center fs-5 fw-bold" role="alert">
-							{reservation.errorMsg}
-							<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-						</div>
-					}
 					<div className="row mb-5">
 						<div className="col-7">
 							<div className="pe-5">
 								{/* <div className="img-banner img-12 rounded"></div> */}
-								<img src={vehiclesServe?.image || defaultImage} alt={vehicle.results[0].name} className='img-banner-5 rounded d-none d-lg-flex'></img>
+								<img src={vehicle.dataDetail?.image || defaultImage} alt={vehicle.dataDetail?.name} className='img-banner-5 rounded d-none d-lg-flex'></img>
 							</div>
 						</div>
 						<div className="col move-1">
-							<div className="pd-bolder">{vehiclesServe.name}</div>
-							<div className="pd-bolder fs-1 fw-light mb-3">{vehiclesServe.location}</div>
+							<div className="pd-bolder">{vehicle.dataDetail?.name}</div>
+							<div className="pd-bolder fs-1 fw-light mb-3">{vehicle.dataDetail?.location}</div>
 							<div
 								className="w-100 border border-3 border-grey rounded p-3 lh-base shadow mb-4"
 							>
 								<div className="px-4">
-									<div className="fs-4">Qt : {counter.num} {vehiclesServe.category}</div>
+									<div className="fs-4">Qt : {reservation.quantity} {vehicle.dataDetail?.category}</div>
 									<div className="fs-4 text-muted fw-bold">No Prepayment</div>
 								</div>
 							</div>
@@ -74,7 +69,7 @@ export const Reservation = () => {
 							>
 								<div className="px-4">
 									<div className="fw-bold fs-4">Details:</div>
-									<div className="fs-4">{counter.num} {vehiclesServe.category} : Rp. {new Intl.NumberFormat('de-DE').format(total)}</div>
+									<div className="fs-4">{reservation.quantity.num} {vehicle.dataDetail?.category} : Rp. {new Intl.NumberFormat('de-DE').format(total)}</div>
 								</div>
 							</div>
 						</div>
--- src/redux/actions/reservation.js
+++ src/redux/actions/reservation.js
@@ -1,4 +1,35 @@
 import http from '../../helpers/http'
+import { RESERVATION_DECQUANTITY, RESERVATION_INCQUANTITY, RESERVATION_RESETDATA, RESERVATION_SETENDDATE, RESERVATION_SETSTARTDATE } from '../reducers/reservation'
+
+export const incQuantity = () =>{
+	return dispatch =>{
+		dispatch({ type : RESERVATION_INCQUANTITY })
+	}
+}
+
+export const decQuantity = () =>{
+	return dispatch =>{
+		dispatch({ type : RESERVATION_DECQUANTITY })
+	}
+}
+
+export const startDate = () =>{
+	return dispatch =>{
+		dispatch({ type : RESERVATION_SETSTARTDATE })
+	}
+}
+
+export const endDate = () =>{
+	return dispatch =>{
+		dispatch({ type : RESERVATION_SETENDDATE })
+	}
+}
+
+export const clearReservation = () =>{
+	return dispatch =>{
+		dispatch({ type : RESERVATION_RESETDATA })
+	}
+}
 
 export const reservation = (data)=>{
 	return{
