ce044b1b4127f0105a1aab2a4a4b7a47b0d3dba8
DanangFirmantara <danangfirmantara837@gmail.com>
2022-05-08 23:16:32 +0700

    feat : add update vehicle admin

--- src/pages/VehicleEdit.js
+++ src/pages/VehicleEdit.js
@@ -1,86 +1,227 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react'
 import LayoutLogin from '../components/LayoutLogin'
-import { getVehicleById } from '../redux/actions/vehicle'
+import {useDispatch, useSelector} from 'react-redux'
+import { Navigate, useParams } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {AiFillCamera} from 'react-icons/ai'
+import {BiTrash} from 'react-icons/bi'
+import LoadingScreen from '../components/LoadingScreen'
+import { resetMsg } from '../redux/actions/resetMsg'
+import { addVehicle, getVehicleById } from '../redux/actions/vehicle'
 
-export const vehicleEdit = () => {
-	const dispatch = useDispatch()
+export const VehicleEdit = () => {
+	const user = useSelector(state=> state.user)
+	const location = useSelector(state=> state.location)
+	const category = useSelector(state=> state.category)
+	const vehicle = useSelector(state => state.vehicle)
+	const auth = useSelector(state=>state.auth)
+
+	const [dataSave, setDataSave] = useState({stock:0})
+	const [image, setImage] = useState()
+	const [error, setError] = useState('')
+
 	const {id} = useParams()
 
-	const vehicle = useSelector(state=> state.vehicle)
+	const dispatch = useDispatch()
+
+	const decreament = (e)=>{
+		e.preventDefault()
+		if(dataSave.stock > 0){
+			setDataSave({...dataSave, stock: dataSave.stock-1})
+		}
+	}
+	const increament = (e)=>{
+		e.preventDefault()
+		setDataSave ({...dataSave, stock: dataSave.stock+1})
+	}
 
 	useEffect(()=>{
+		dispatch(resetMsg())
 		if(vehicle.dataDetail.id !== parseInt(id)){
 			dispatch(getVehicleById(id))
 		}
 	},[dispatch])
 
+	const fileInputHandler = (e)=>{
+		e.preventDefault()
+		const reader = new FileReader()
+		setDataSave({...dataSave, image: e.target.files[0]})
+		const image = e.target.files[0]
+		reader.readAsDataURL(image)
+		reader.onload =(e)=>{
+			setImage(e.target.result)
+		}
+	}
+
+	const onCreate = (event)=>{
+		event.preventDefault()
+		setError('')
+		const name = event.target.elements['name'].value
+		const idLocation  = event.target.elements['location'].value
+		const description = event.target.elements['description'].value
+		const price = event.target.elements['price'].value
+		const idCategory = event.target.elements['category'].value
+		const data = {...dataSave,name, idLocation, description, price, idCategory}
+		let n = 7
+		Object.keys(data).forEach((item)=>{
+			if(data[item] === ''){
+				if(item === 'idCategory'){
+					return setError('Select category')
+				}
+				if( item === 'idLocation'){
+					return setError('Select location')
+				}
+				return setError(`Please fill ${item}`)
+			} else {
+				n--
+			}
+		})
+		if(n <= 0){
+			console.log(data)
+			// dispatch(addVehicle(data, auth.token))
+		} else{
+			return setError('Please Add image')
+		}
+	}
 	return (
 		<LayoutLogin>
-			<main>
-				<div className="container">
-					<a className="d-flex align-items-center mb-5" href="./vehicle-type.html">
-						<i className="fa-solid fa-chevron-left icon dark fs-0 me-3"></i>
-						<div className="fs-1 fw-bold text-dark">Detail</div>
-					</a>
-					<div className="row mb-5">
-						<div className="col-7 g-0">
-							<div className="pe-5">
-								<div className="mb-5">
-									<div className="img-banner img-12 rounded"></div>
+			{user.data?.role !== 'Admin' && (
+				<Navigate to='/' />
+			)}
+			{user.isLoading || location.isLoading || category.isLoading || vehicle.isLoading ? (
+				<LoadingScreen />
+			) : (
+				<main className='my-5'>
+					<div className="container">
+						<div className='my-4'>
+							{error !== '' &&
+								(
+									<div className="alert button-third shadow-dark alert-dismissible fade show text-center fs-5 fw-bold" role="alert">
+										{error}
+										<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=> setError('')}></button>
+									</div>
+								)
+							}
+							{vehicle.successMsg !== '' &&
+								(
+									<div className="alert button-third shadow-dark alert-dismissible fade show text-center fs-5 fw-bold" role="alert">
+										{vehicle.successMsg}
+										<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=> dispatch(resetMsg())}></button>
+									</div>
+								)
+							}
+							{vehicle.errorMsg !== '' &&
+								(
+									<div className="alert button-third shadow-dark alert-dismissible fade show text-center fs-5 fw-bold" role="alert">
+										{vehicle.errorMsg}
+										<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=> dispatch(resetMsg())}></button>
+									</div>
+								)
+							}
+						</div>
+						<div className='d-flex align-items-center mb-4'>
+							<Link className="fa-solid fa-chevron-left icon dark fs-0 me-5" to='/'></Link>
+							<div className="fs-1 fw-bold text-dark">Edit vehicles</div>
+						</div>
+						<form id="create" onSubmit={onCreate}>
+							<div className="row mb-5">
+								<div className="col-7 g-0">
+									<div className="pe-5">
+										<div className="mb-5 position-relative d-flex ">
+											{vehicle.dataDetail?.image ? (
+												<img className='img-banner-5 rounded' src={vehicle.dataDetail.image}/>
+											) : (
+												<AiFillCamera className='img-banner-5 rounded bg-grey text-third'/>
+											)}
+											<div className='position-absolute end-0'>
+												<button className="icon-circle-3 rounded-circle button-fourth justify-content-center d-flex align-items-center position-relative">
+													{image ? (
+														<button 
+															onClick={()=> {
+																setImage()
+																delete dataSave.image
+															}} 
+															className='bg-transparent border-0'>
+															<BiTrash />
+														</button>
+													) : (
+														<>
+															<i className="fa-solid fa-pencil"/>
+															<input type='file' name='image' className='position-absolute input-img opacity-0' onChange={(e)=> fileInputHandler(e)}/>
+														</>
+													)}
+												</button>
+											</div>
+										</div>
+										<div className="d-flex justify-content-around align-items-center">
+											<AiFillCamera className='img-thumbnail rounded bg-grey'/>
+											<AiFillCamera className='img-thumbnail rounded bg-grey'/>
+										</div>
+									</div>
 								</div>
-								<div className="d-flex justify-content-between align-items-center">
-									<i className="fa-solid fa-chevron-left icon dark"></i>
-									<div className="img-slide img-12 rounded"></div>
-									<div className="img-slide img-12 rounded"></div>
-									<i className="fa-solid fa-chevron-right icon dark"></i>
+								<div className="col">
+									<input className='w-100 pb-2 border-0 border-bottom border-3 border-dark fs-4 fw-light mb-4' placeholder='Name (max up to 50 words) ' type='text' name='name' defaultValue={vehicle.dataDetail?.name}/>
+									<div className='w-100 pb-2 border-0 border-bottom border-3 border-dark  mb-4 position-relative d-flex align-items-center' placeholder='Location' type='text' name='location'>
+										<select className='w-100 border-0 fs-4 fw-light text-muted form-select py-0 px-0' type='text' placeholder='Select location' name='location'>
+											<option style={{display : 'none'}} className='py-2' value='' selected={vehicle.dataDetail?.idLocation}>Select location</option>
+											{location.data.length > 0 && location.data.map((obj)=>{
+												return (
+													<option key={obj.name} selected={vehicle.dataDetail?.idLocation === obj.id} className='border border-2 text-dark' value={obj.id}>{obj.name}</option>
+												)
+											})}
+										</select>
+										<div className='fa-solid fa-chevron-down position-absolute fs-2 end-0 mx-2 custom-select'></div>
+									</div>
+									<input className='w-100 pb-2 border-0 border-bottom border-3 border-dark fs-4 fw-light mb-4' placeholder='Description (max up to 150 words)' type='text' name='description' defaultValue={vehicle.dataDetail?.description}/>
+									<div className='pd-heading fs-4 mb-3' defaultValue={vehicle.dataDetail?.price}>Price :</div>
+									<input className='w-100 py-4 px-3 border-0 bg-grey rounded fs-4 fw-light mb-4' type='text' placeholder='Type the price' name='price' defaultValue={vehicle.dataDetail?.price}/>
+									<div className='pd-heading fs-4 mb-3'>Status :</div>
+									<div className='d-flex position-relative align-items-center mb-5'>
+										<div className='w-100 py-4 px-3 border-0 bg-grey rounded fs-4 fw-light'>
+											<div className={dataSave.stock === 0 ? 'text-danger' : 'text-success'}>
+												{dataSave.stock === 0 ? 'Full Booked' : 'Available'}
+											</div>
+										</div>
+									</div>
+									<div className="d-flex justify-content-between">
+										<button className="icon-plus button-dark rounded bg-yellow fw-bolder fs-1" onClick={(e)=>decreament(e)}>-</button>
+										<div name='stock' className="fw-bolder fs-0">
+											{vehicle.dataDetail?.stock ? vehicle.dataDetail.stock : dataSave.stock}
+										</div>
+										<button className="icon-plus rounded bg-yellow fw-bolder fs-1" onClick={(e)=>increament(e)}>+</button>
+									</div>
+							
 								</div>
 							</div>
-						</div>
-						<div className="col">
-							<h1 className="pd-bolder mb-3">{vehicle.dataDetail?.name}</h1>
-							<h3 className="pd-bolder fw-normal fs-2 mb-3">Yogyakarta</h3>
-							<div className="fs-4 green fw-bold">Available</div>
-							<div className="fs-4 text-danger fw-light mb-3">No prepayment</div>
-							<ul className="lh-base fs-4 fw-light mb-4">
-								<li>Capacity : 1 Person</li>
-								<li>Type : Bike</li>
-								<li>Reservation before 2 PM</li>
-							</ul>
-							<div className="pd-bolder fs-1 d-flex justify-content-center pt-2 mb-6">
-              Rp. 78.000/day
+							<div className="d-flex justify-content-between row">
+								<div className="col-5">
+									<div className='d-flex position-relative align-items-center'>
+										<select className="button-height button-dark w-100 fw-bolder fs-4 shadow-dark form-select text-center" name='category'>
+											<option style={{display : 'none'}} className='py-2' value=''>Select category</option>
+											{category.data.length > 0 && category.data.map((obj)=>{
+												return (
+													<option key={obj.name} className='border border-2' selected={vehicle.dataDetail?.idCategory === obj.id} value={obj.id}>{obj.name}</option>
+												)
+											})}
+										</select>
+										<div className='fa-solid fa-chevron-down position-absolute fs-2 end-0 me-5 custom-select yellow'></div>
+									</div>
+								</div>
+								<div className="col">
+									<button className="button-height button-yellow w-100 fw-bolder fs-4 shadow-yellow" type='submit'>
+                        Save item
+									</button>
+								</div>
 							</div>
-							<div className="d-flex justify-content-between">
-								<button className="icon-plus rounded bg-yellow">+</button>
-								<div className="fw-bolder fs-0">2</div>
-								<button className="icon-plus rounded bg-yellow">+</button>
-							</div>
-						</div>
+						</form>
 					</div>
-					<div className="d-flex justify-content-between row">
-						<div className="col-5">
-							<button className="button-height button-dark w-100 fw-bolder fs-4 shadow-dark">
-              Chat Admin
-							</button>
-						</div>
-						<div className="col-4">
-							<a href="/reservation.html">
-								<button className="button-height button-yellow w-100 fw-bolder fs-4 shadow-yellow">
-                Reservation
-								</button>
-							</a>
-						</div>
-						<div className="col-3">
-							<button className="button-height button-dark w-100 fw-bolder fs-4 shadow-dark">
-								<div className="d-flex align-items-center justify-content-center"><i className="fa-solid fa-heart me-4 fs-0"></i>Like</div>
-							</button>
-						</div>
-					</div>
-				</div>
-			</main>
+				</main>
+			)}
+			
 		</LayoutLogin>
 	)
 }
 
-export default vehicleEdit
+export default VehicleEdit
+
